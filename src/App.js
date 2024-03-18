56c992d27b411b739b076545526b928bc57bf870
ABHISHEK <abhisheksur99@gmail.com>
2024-03-18 09:56:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import useData from './utils/useData.js';
+import Table from './Table.js';
+import './App.css';
+
+function App() {
+  const data = useData();
+  return (
+    <div className="App">
+      <main>
+      <Table data={data} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
